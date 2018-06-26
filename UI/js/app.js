3358eb0d3e888402012d6f989d1105d43a2c9dac
justinembawomye <jmbawomye@gmail.com>
2018-06-26 09:15:02 +0300

    add javascript alert

---
+++ UI/js/app.js
@@ -0,0 +1,10 @@
+var login = document.querySelector('.admin-login');
+login.onclick = function(){
+    alert('You are logged in as admin. \
+    Press OK to continue');
+}
+
+var userLogin = document.querySelector('.user-login');
+userLogin.onclick = function(){
+    alert('You are logged in as a user');
+}
