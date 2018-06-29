c05161593efa247327b96cda96ada4f327d7835a
justinembawomye <jmbawomye@gmail.com>
2018-06-29 11:50:07 +0300

    create register and login endpoints

--- api/users/views.py
+++ api/users/views.py
@@ -1,8 +1,9 @@
 
 from api import app
 from flask import request, jsonify
+from .models import User, users
 
-users = []
+
 
 @app.route('/auth/register', methods=['POST'])
 def register_user():
@@ -11,20 +12,46 @@ def register_user():
 
     if not user_data:
         return jsonify({'message': 'All fields are required'}), 400
-
+    name = user_data.get('name')
     email = user_data.get('email')
-    password = user_data.get('password')
     username = str(user_data.get('username')).strip()
+    password = user_data.get('password')
 
     # validate user data
+    if not name:
+        return jsonify({'message': 'name is required'}), 400
+
+    if not email:
+        return jsonify({'message': 'Email address is required'}), 400     
+
     if not username:
-        return jsonify({'message': 'Username is required'}), 400
-    
+        return jsonify({'message': 'username is required'}), 400 
+
     user_data['id'] = len(users)
-    # store your data to your database
+    # Add users
     users.append(user_data)
 
     return jsonify({ 'message': f'User {username} has been registered'}), 201
+
+
+@app.route('/auth/login', methods=['POST'])
+def login_user():
+    # getting user data
+    user_data = request.get_json()
+
+    if not user_data:
+        return jsonify({'Missing': 'These fields are required'}), 400
+    username = str(user_data.get('username')).strip()
+    password = user_data.get('password')
+
+    # validate user data
+    if not username:
+        return jsonify({'message': 'username is required'}), 400 
+
+    if not password:
+        return jsonify({'message': 'password missing! This field is required'}), 400     
+    return jsonify({ 'message': f'Welcome {username} You are logged in'}), 200
+
 
 @app.route('/users')
 def get_all_users():
