13d8acb4647f64c6b5a6b03919b67d68a00a389d
justinembawomye <jmbawomye@gmail.com>
2018-07-04 22:06:09 +0300

    test user login

--- tests/test_users.py
+++ tests/test_users.py
@@ -9,7 +9,12 @@ class UserTestCase(BaseTestCase):
         self.assertEqual(response.status_code, 201)
         self.assertIn("User Tinah has been registered",str(response.data))
 
-   
+    def test_user_login(self):
+        response = self.test_client.post('/auth/login', data=json.dumps(self.user_data), content_type = 'application/json')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("Welcome Tinah. You are logged in",str(response.data))
+
+        
 
      
 
