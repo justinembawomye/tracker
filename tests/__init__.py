55a1dc110b72977e6918c3b777e2880d061bb369
justinembawomye <jmbawomye@gmail.com>
2018-07-04 22:31:05 +0300

    add dummy data for requests

--- tests/__init__.py
+++ tests/__init__.py
@@ -8,12 +8,21 @@ class BaseTestCase(unittest.TestCase):
     def setUp(self):
         self.test_client = app.test_client()
         self.user_data = {
-            "id": 1,
+            "user_id": 1,
             "name": "Justine",
             "email":"justine@gmail.com",
             "username":"Tinah",
             "password":"123456"
            
+        }
+        self.request_data = {
+            "request_id":1,
+            "client_name":"kiz",
+            "email":"deekiz@gmail.com",
+            "category":"repair",
+            "request_title":"please repair my pc",
+            "description":"Hey! could you please repair my pc by noon",
+            "department":"control"
         }
 
     
