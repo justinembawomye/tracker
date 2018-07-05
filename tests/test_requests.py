f209a638b703f6bb052b24495d287cb00245d34f
justinembawomye <jmbawomye@gmail.com>
2018-07-05 10:24:33 +0300

    test  if a user can craeate a request

--- tests/test_requests.py
+++ tests/test_requests.py
@@ -1,1 +1,15 @@
+from tests import BaseTestCase
+import json
+
+
+class RequestTestCase(BaseTestCase):
     
+    def test_create_request(self):
+        """ Tests whether a user can create a request successfully """
+
+        response = self.test_client.post('/api/v1/users/requests', data=json.dumps(self.request_data), content_type = 'application/json')
+        self.assertEqual(response.status_code, 201)
+        self.assertIn("Hey kiz! You have successfully created a request",str(response.data))
+
+    
+
