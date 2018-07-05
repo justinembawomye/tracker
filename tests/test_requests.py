a49e73ba1f15d2b03a849d86361640f63660e702
justinembawomye <jmbawomye@gmail.com>
2018-07-05 11:24:30 +0300

    test  fetch all requests for a user

--- tests/test_requests.py
+++ tests/test_requests.py
@@ -11,5 +11,13 @@ class RequestTestCase(BaseTestCase):
         self.assertEqual(response.status_code, 201)
         self.assertIn("Hey kiz! You have successfully created a request",str(response.data))
 
+    def test_get_all_requests(self):
+        """ Tests whether a user fetch all his/her requests successfully """
+
+        response = self.test_client.post('/api/v1/users/requests', data=json.dumps(self.request_data), content_type = 'application/json')
+        response = self.test_client.get('/api/v1/users/requests', data=json.dumps(self.request_data), content_type = 'application/json')
+        self.assertEqual(response.status_code, 302)
+          
+
     
 
