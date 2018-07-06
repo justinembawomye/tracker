374065f619e2c8c73dc55a9b8d36dabbecac47f3
justinembawomye <jmbawomye@gmail.com>
2018-07-06 21:29:17 +0300

    modify edit request route

--- api/views.py
+++ api/views.py
@@ -46,7 +46,7 @@ def login_user():
     username = str(user_data.get('username')).strip()
     password = user_data.get('password')
 
-    return jsonify({"message":"Welcome Tinah. You are logged in"})
+    return jsonify({"message": "Welcome Tinah. You are logged in"})
 
 
 @app.route('/users')
@@ -79,7 +79,7 @@ def create_request():
     # request_time = datetime.datetime.now()
 
     # validate request data
-    if not client_name or client_name == " " or len(client_name < 3) or client_name == type(int):
+    if not client_name or client_name == " " or client_name == type(int):
         return jsonify({'message': 'client name is required'}), 400
 
     if not email or email == " ":
@@ -91,21 +91,9 @@ def create_request():
         return jsonify({
             'status': 'Required',
             'message': 'request title field is missing'}), 400
-<<<<<<< HEAD
 
-<<<<<<< HEAD
-    if not description or description == " " or len(description < 10):
+    if not description or description == " ":
         return jsonify({'message': 'Description is required'}), 400
-=======
-    if not department:
-        return jsonify({'message': 'department is required'}), 400 
->>>>>>> challenge2
-=======
- ft-oop
-    if not description or description == " " or len(description < 10):
-        return jsonify({'message': 'Description is required'}), 400
-
->>>>>>> 365068b68899e62ac96c8024d8847bb8751ab5a5
 
     if not department or department == " ":
         return jsonify({
@@ -142,35 +130,18 @@ def get_single_request(request_id):
         'message': 'That request is doesn\'t exist'}), 404
 
 
-@app.route("/api/v1/users/requests/<int:requestId>", methods=['PUT'])
+@app.route("/api/v1/users/requests/<int:request_id>", methods=['PUT'])
 def update_request(request_id):
-    """ Endpoint to edit a user request"""
-
-    # check if user has any requests
-    if len(requests) < 0:
-        return jsonify({
-            "status": "Not found",
-            "message": "You haven't made any requests"
-        }), 400
-
-    # if user has more than one request
-    if len(requests) >= 1:
-        # get entered data
-        data = request.get_json()
-
-        # picking the request attributes
-        client_name = data.get("client_name")
-        email = data.get("email")
-
-        if len(requests) >= 1:
-            for single_request in requests:
-                if single_request.request_id == int(request_id):
-                    single_request.client_name = client_name
-                    single_request.email = email
-                    single_request.category = category
-                    single_request.request_title = request_title
-                    single_request.department = department
-                    single_request.description = description
-                    return jsonify({
-                        "message": "Successfully edited the request"
-                    })
+    """ Endpoint to edit a request"""
+    new_request_data = request.get_json()
+    for i in requests:
+        if i:
+            client_name = new_request_data.get('client_name')
+            email = new_request_data.get('email')
+            category = new_request_data.get('category')
+            request_title = new_request_data.get('request_title')
+            description = new_request_data.get('description')
+            department = new_request_data.get('department')
+        return jsonify({"message":"Request updated successfully"}),200 
+    return jsonify({"message":f"Failed to update request"})
+            
