e3bd663dccb8897e4e5d219c38f1670de62ce3c8
justinembawomye <jmbawomye@gmail.com>
2018-07-10 08:24:46 +0300

    test create account failure

--- tests/test_users.py
+++ tests/test_users.py
@@ -9,6 +9,16 @@ class UserTestCase(BaseTestCase):
         self.assertEqual(response.status_code, 201)
         self.assertIn("User Tinah has been registered",str(response.data))
 
+
+
+    def test_register_user_fail(self):
+        """Ensures that a user with missing credentials is not registered"""
+        response = self.test_client.post('/auth/register', content_type='application/json',
+                            data=json.dumps({"email":"justine@gmail.com","username":"justine","password":"123456"}))
+        self.assertEqual(response.status_code, 400)
+    
+    
+
     def test_user_login(self):
         response = self.test_client.post('/auth/login', data=json.dumps(self.user_data), content_type = 'application/json')
         self.assertEqual(response.status_code, 200)
